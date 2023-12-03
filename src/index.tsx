bfdcf5c4abe6ddbc887f4512b27bcad15610794c
ShahSwapnil <SwapnilShah99@gmail.com>
2023-12-03 10:13:40 -0500

    Add Bootstrap and SCSS support

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './simple-budget-ui.scss'
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
